266f870933e508cc901626bbb50698daa9fa9c62
maitc <maibeet.torres@gmail.com>
2017-07-10 17:17:35 -0400

    Etapa 8: se agrega clase con addclass y se remueve clase con removeclass

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-	//ETAPA 8: Se imprime mensaje por consola.
+	//ETAPA 8: Se imprime mensaje por consola.(me equivoqué al escribir el commit por eso hay dos Etapa 6)
 	console.log("SE CARGÓ MAMITA");
 	$(".js-menu").hide();
+})
+
+//ETAPA 8: agrega y saca clase
+$(".js-show-make").click(function(){
+	$(".page").addClass("make");
+
+})
+$(".js-show-recipe").click(function(){
+	$(".recipe").removeClass("make");
 })
