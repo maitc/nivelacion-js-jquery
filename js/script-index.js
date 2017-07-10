1b8d640d1d589ed8c43b98f29aea4cb5f5a4f86e
maitc <maibeet.torres@gmail.com>
2017-07-10 16:52:20 -0400

    Etapa 6: se realiza for each para crear por jquery la informacion del objeto y forma el div de actividades

--- js/script-index.js
+++ js/script-index.js
@@ -26,7 +26,7 @@ function renderHighlightedRecipes(recipesArray) {
 		if(e.highlighted == true){
 			renderRecipe(e);
 			//ETAPA 4: solo titulos y autores
-			$(".list-recipes").append("<img src= 'img/recipes/320x350/"+ $(e).attr("name") +".jpg' class='img-recipes'><h3 class='title-recipes'>" + e.title  + "</h3><p class='author-recipes'>" + e.source.name + "</p>");
+			$(".list-recipes").append("<a class='item-recipe' href='#'><span class='attribution'><span class='title-recipe'>" +  e.title + "</span><span class='metadata-recipe'><span class='author-recipe'>" + e.source.name + "</span><span class='bookmarks-recipe'><span class='icon-bookmark'></span> </span></span></span><img src='img/recipes/320x350/"+ $(e).attr("name") +".jpg'></a>");
 		}
 	})
 }
@@ -49,6 +49,8 @@ function renderRecipe(recipe) {
 */
 function renderActivities(activitiesArray) {
 	console.log('Activities: ', activitiesArray);
+	//ETAPA 5: ocultar div.
+	
 	if(activitiesArray.length > 0){
 		$(".wrapper-message").hide();
 	}
@@ -59,8 +61,12 @@ function renderActivities(activitiesArray) {
 * Aqui se tiene que crear el HTML que esta en el 
 * archivo "templates/templates-activity.html"
 */
+renderActivity(activitiesArray);
 function renderActivity(recipe) {
-	
+	//ETAPA 6: actividad
+	recipe.forEach(function(e){
+		$(".list-activities").append("<a href='#'' class='item-activity'><span class='attribution'><span class='avatar'><img src='" + e.userAvatar + "'class='image-avatar'></span><span class='meta'><span class='author'>" + e.userName + "</span> made <span class='recipe'>" + e.recipeName + "</span>: " + e.text + "<span class='location'>" + e.place +"</span></span></span><div class ='bg-image' style='background-image:url("+ $(e).attr("image") + ");'></div></a>"); 
+	})
 }
 
 
