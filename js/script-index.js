43182c7ee5721d8fff501260e11709c651e6305b
maitc <maibeet.torres@gmail.com>
2017-07-10 11:01:54 -0400

    Etapa 1: se realiza hide para ocultar iconos especificados para desaparecer en esta etapa

--- js/script-index.js
+++ js/script-index.js
@@ -2,6 +2,8 @@ $(document).ready( function(){
 
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
+	//ocultando flecha 
+	$(".js-back").hide();
 
 });
 
